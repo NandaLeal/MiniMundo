62064ac9cc7cad368fc9ef9a6b60055cbd04d901
Filip3ra <filipimacielrj@gmail.com>
2021-07-09 11:48:33 -0300

    Atributos, getters e setters implementados.

--- ProjetoEstacionamento/src/main/java/br/edu/ifnmg/projetoPOO/Principal.java
+++ ProjetoEstacionamento/src/main/java/br/edu/ifnmg/projetoPOO/Principal.java
@@ -1,16 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.edu.ifnmg.projetoPOO;
+
+import java.util.Scanner;
 
 /**
  *
- * @author Filip
+ * @author Filipi
+ * @version 1.1
  */
 public class Principal {
+    
+    public static Usuario user1 = new Usuario();
+    
     public static void main(String[] args) {
         
+        /*
+            Tudo que foi implementado na classe Principal
+            é apenas pra teste. Geters e Seters de Usuário 
+            estão funcionando bem.
+        */
+        
+        Scanner scan = new Scanner(System.in);  // Invoco a função Scanner pra ler input do teclado
+        String senhaTeclado;
+        
+        // Set dos atributos de um usuário
+        user1.setNome("Filipi");
+        user1.setAdmin(true);
+        user1.setDdd(38);
+        user1.setEmail("fmrj@aluno");
+        user1.setFone(992160517);
+        
+        System.out.println("Informe a senha: ");        
+        senhaTeclado = scan.nextLine(); // Leio input do teclado, guardo em "senhaTeclado"
+        user1.setSenha(senhaTeclado);
+        
+        System.out.println(user1.getSenha());
+        System.out.println(user1.getNome());
+        System.out.println(user1.getFone());
+        System.out.println(user1.getDdd());
+        System.out.println(user1.getEmail());
+        
+    
     }
 }
--- ProjetoEstacionamento/src/main/java/br/edu/ifnmg/projetoPOO/Vaga.java
+++ ProjetoEstacionamento/src/main/java/br/edu/ifnmg/projetoPOO/Vaga.java
@@ -1,16 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.edu.ifnmg.projetoPOO;
 
 /**
  *
  * @author Filipi
- * @version 1.0
+ * @version 1.1
  */
 public class Vaga {
     
     private int numero = 0;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
 }
